63c28fe45c097f6ab76a9110604aa78fd1f3792b
bilalahmed1 <bilal.ahmed@wistech.biz>
2024-07-24 18:08:06 +0500

    boat details view in enquiry added.

--- vendor/naeemawan/predefinedlists/resources/views/customizeboat/enquiry.blade.php
+++ vendor/naeemawan/predefinedlists/resources/views/customizeboat/enquiry.blade.php
@@ -4,99 +4,10 @@
     <p>User Phone Number: <b><i>{{ $boat_enquiry->customer->phone }}</i></b></p>
     <p>Submitted At: <i>{{ $boat_enquiry->created_at }}</i></p>
     <p>Boat Name: <b><i>{{ $boat_enquiry->boat->ltitle }}</i></b></p>
-    <p>Boat Customization Details: <b><i>{{ $boat_enquiry->message }}</i></b></p>
+    <p>Boat Customization Details: <b><i>{{ $boat_enquiry->message ?? '' }}</i></b></p>
     <div class="row" id="summary-end">
-	  <div class="col-12 m-auto">
-	    <div class="card card-custom">
-	      <div class="card-body summary-card justify-content-center d-flex flex-row flex-wrap">
-	      	@if($boat_enquiry->details)
-			@foreach($boat_enquiry->details as $key=>$value)
-	      	<div class="card m-1">
-	      		<div class="card-body text-center">
-	      			<p><b>{{$value->slug->ltitle ?? ''}}: </b>{{$value->slug->parent->ltitle ?? ''}}</p>
-	      			<p><b>Price</b> : {{ format_price($value->enquiry_option->price) }}</p>
-	      		</div>
-	      	</div>
-			@endforeach
-	      	@endif
-	      </div>
-	    </div>
-		<!-- Final image -->
-		<div class="row">
-                        <div id="carouselExampleControls" class="custom-boat carousel slide">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-							@foreach($boat_enquiry->details as $key=>$value)
-                            @if(isset($value->enquiry_option->image[1]))
-                                <div style="position:absolute;"  >
-                                    <img src="{{ RvMedia::getImageUrl($value->enquiry_option->image[1], '', false, RvMedia::getDefaultImage()) }}" class="d-block w-100" alt="...">
-                                </div>
-                            @endif
-                            @endforeach
-                            <div class="" style="z-index:0">
-                                <img src="{{ RvMedia::getImageUrl($boat_enquiry->boat->image[1] ?? $boat_enquiry->image, '', false, RvMedia::getDefaultImage()) }}" class="d-block w-100" alt="...">
-                            </div>
-                            </div>
-
-                            <div class="carousel-item">
-                            @foreach($boat_enquiry->details as $key=>$value)
-                            @if(isset($value->enquiry_option->image[2]))
-                                <div style="position:absolute;"  >
-                                    <img src="{{ RvMedia::getImageUrl($value->enquiry_option->image[2], '', false, RvMedia::getDefaultImage()) }}" class="d-block w-100" alt="...">
-                                </div>
-                            @endif
-                            @endforeach
-                            <div class="" style="z-index:0">
-                                <img src="{{ RvMedia::getImageUrl($boat_enquiry->boat->image[2] ?? $boat_enquiry->image, '', false, RvMedia::getDefaultImage()) }}" class="d-block w-100" alt="...">
-                            </div>
-                            </div>
-
-                            <div class="carousel-item">
-							@foreach($boat_enquiry->details as $key=>$value)
-                            @if(isset($value->enquiry_option->image[3]))
-                                <div style="position:absolute;"  >
-                                    <img src="{{ RvMedia::getImageUrl($value->enquiry_option->image[3], '', false, RvMedia::getDefaultImage()) }}" class="d-block w-100" alt="...">
-                                </div>
-                            @endif
-                            @endforeach
-                            <div class="" style="z-index:0">
-                                <img src="{{ RvMedia::getImageUrl($boat_enquiry->boat->image[3] ?? $boat_enquiry->image, '', false, RvMedia::getDefaultImage()) }}" class="d-block w-100" alt="...">
-                            </div>
-                            </div>
-
-                            @if(isset($boat_enquiry->boat->image[4]))
-                            <div class="carousel-item">
-							@foreach($boat_enquiry->details as $key=>$value)
-                            @if(isset($value->enquiry_option->image[4]))
-                                <div style="position:absolute;"  >
-                                    <img src="{{ RvMedia::getImageUrl($value->enquiry_option->image[4], '', false, RvMedia::getDefaultImage()) }}" class="d-block w-100" alt="...">
-                                </div>
-                            @endif
-                            @endforeach
-                            <div class="" style="z-index:0">
-                                <img src="{{ RvMedia::getImageUrl($boat_enquiry->boat->image[4] ?? $boat_enquiry->image, '', false, RvMedia::getDefaultImage()) }}" class="d-block w-100" alt="...">
-                            </div>
-                            </div>
-                            @endif
-
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-        </div>
-
-        <div class="card-body">
-        <h6>Included:</h6>
-      {!! $boat_enquiry->boat->detail->standard_options !!}
-      </div>
-                    
-                   
+        <!-- start -->
+                <!-- the selected options will be shown here -->
 		<!-- end -->
 	    <div class="card-footer">
 	      <div class="row m-2">
